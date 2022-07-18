1866bd3058f55e35f1465b87a004ba3d3c6b127e
Cal <callum.adair@outlook.com>
2022-07-18 20:23:35 +0100

    Added the images and user relationships to the item model.

--- app/Models/Item.php
+++ app/Models/Item.php
@@ -4,14 +4,25 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Item extends Model
 {
     use HasFactory;
 
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
     public function issues(): HasMany
     {
         return $this->hasMany(Issue::class);
+    }
+
+    public function images(): HasMany
+    {
+        return $this->hasMany(Image::class);
     }
 }
