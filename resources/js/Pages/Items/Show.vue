d268021e997a3fa0718dd55c2ea03cb38aa4ccaa
Cal <callum.adair@outlook.com>
2022-07-18 23:12:21 +0100

    Initial skeleton of item/show.vue

---
+++ resources/js/Pages/Items/Show.vue
@@ -0,0 +1,13 @@
+<script setup>
+import AppLayout from '@/Layouts/AppLayout.vue';
+
+defineProps({
+    item: Object
+});
+</script>
+
+<template>
+    <AppLayout title="{{$page.props.item.name}}">
+
+    </AppLayout>
+</template>
