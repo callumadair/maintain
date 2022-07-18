0851342606431de6e3b6f603859102cc76bb8ff8
Cal <callum.adair@outlook.com>
2022-07-18 20:22:34 +0100

    Created image migration with appropriate foreign keys.

---
+++ database/migrations/2022_07_18_191350_create_images_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('images', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('image_path');
+
+            $table->foreignId('item_id')->nullable();
+            $table->foreign('item_id')->references('id')
+                ->on('items')->onDelete('cascade')->onUpdate('cascade');
+
+            $table->foreignId('issue_id')->nullable();
+            $table->foreign('issue_id')->references('id')
+                ->on('issues')->onDelete('cascade')->onUpdate('cascade');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('images');
+    }
+};
