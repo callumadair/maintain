724fa742c16d8b848b8a43c30c5d28b467975262
Cal <callum.adair@outlook.com>
2022-07-18 20:23:21 +0100

    Added the images relationship to the issue model.

--- app/Models/Issue.php
+++ app/Models/Issue.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Issue extends Model
 {
@@ -19,6 +20,11 @@ class Issue extends Model
     public function users(): BelongsToMany
     {
         return $this->belongsToMany(User::class);
+    }
+
+    public function images(): HasMany
+    {
+        return $this->hasMany(Image::class);
     }
 
 }
