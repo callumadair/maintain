3cf87292a2ca545d5b2ce39f47e749e8169c30f0
Cal <callum.adair@outlook.com>
2022-07-19 23:39:16 +0100

    Added a link to the itemcard component.

---
+++ resources/js/Pages/Items/Partials/ItemCard.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { Link } from '@inertiajs/inertia-vue3';
+
+defineProps({
+    item: Object,
+});
+</script>
+
+<template>
+    <div>
+        <Link>{{ item.name }}</Link>
+    </div>
+</template>
