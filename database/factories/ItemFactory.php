11ab6893751a9d422a77e4ccb3d0764ded2151ca
Cal <callum.adair@outlook.com>
2022-07-19 15:00:39 +0100

    Initial attempt at item factory

--- database/factories/ItemFactory.php
+++ database/factories/ItemFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -17,7 +18,8 @@ class ItemFactory extends Factory
     public function definition()
     {
         return [
-            //
+            'name' => "Box",
+            'user_id' => User::all()->random()->id,
         ];
     }
 }
