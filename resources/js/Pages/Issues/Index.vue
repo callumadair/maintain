f07bc4da6f392c4d41472a695c638d2e8d4a6ba8
Cal <callum.adair@outlook.com>
2022-07-21 13:58:04 +0100

    Setup basic index component.

--- resources/js/Pages/Issues/Index.vue
+++ resources/js/Pages/Issues/Index.vue
@@ -1,20 +1,29 @@
 <script setup>
 import AppLayout from '@/Layouts/AppLayout.vue';
-import Welcome from '@/Jetstream/Welcome.vue';
+import IssueCard from '@/Pages/Issues/Partials/IssueCard.vue';
+
+defineProps({
+    issues: Object,
+});
 </script>
 
 <template>
     <AppLayout title="Issue Log">
         <template #header>
             <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                All current issues
+                Issue Log
             </h2>
         </template>
 
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                    <Welcome />
+                    <ul>
+                        <li v-for="issue in issues">
+                            issue.name
+                            <IssueCard :issue="issue"/>
+                        </li>
+                    </ul>
                 </div>
             </div>
         </div>
