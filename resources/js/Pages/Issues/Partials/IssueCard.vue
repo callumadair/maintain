261fd0af43b974384da74455b1bad7adb5ee67eb
Cal <callum.adair@outlook.com>
2022-07-21 13:58:30 +0100

    Setup basic issue card component.

---
+++ resources/js/Pages/Issues/Partials/IssueCard.vue
@@ -0,0 +1,17 @@
+<script setup>
+import {Link} from '@inertiajs/inertia-vue3';
+
+defineProps({
+    issue: Object,
+});
+</script>
+
+<template>
+    <div>
+        <Link :href="route('issues.show', issue.id)" :data="issue">
+            <div class="text-center p-3 hover:bg-gray-50 hover:text-indigo-400">
+                {{ issue.name }}
+            </div>
+        </Link>
+    </div>
+</template>
