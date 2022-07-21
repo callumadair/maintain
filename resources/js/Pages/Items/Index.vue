24cabd6bc1a188392f248931756391be91136b4d
Cal <callum.adair@outlook.com>
2022-07-21 22:34:24 +0100

    Reduced style width.

--- resources/js/Pages/Items/Index.vue
+++ resources/js/Pages/Items/Index.vue
@@ -16,7 +16,7 @@ defineProps({
         </template>
 
         <div class="py-12">
-            <div class="max-w-8xl mx-auto sm:px-6 lg:px-8">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
                     <ul>
                         <li v-for="item in items">
