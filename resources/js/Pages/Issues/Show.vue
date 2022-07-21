33440f52ec113cec82f708ccfa04273964d71729
Cal <callum.adair@outlook.com>
2022-07-21 14:45:10 +0100

    Styling.

--- resources/js/Pages/Issues/Show.vue
+++ resources/js/Pages/Issues/Show.vue
@@ -17,8 +17,8 @@ defineProps({
 
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                    Hello, world!
+                <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg p-2 text-center">
+                    {{ issue.description }}
                 </div>
             </div>
         </div>
