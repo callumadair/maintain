b3202db22d8c9db2d693f690acf1bbe6bee269c4
Cal <callum.adair@outlook.com>
2022-07-23 15:13:11 +0100

    Implemented basic create method.

--- app/Http/Controllers/IssueController.php
+++ app/Http/Controllers/IssueController.php
@@ -33,11 +33,11 @@ class IssueController extends Controller
     /**
      * Show the form for creating a new resource.
      *
-     * @return Response
+     * @return \Inertia\Response
      */
-    public function create(): Response
+    public function create(): \Inertia\Response
     {
-        //
+        return Inertia::render('Issues/Create');
     }
 
     /**
