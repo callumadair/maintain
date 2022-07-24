73ef76e6813fd9d2b4dd8a5cd2e57ef2ae6cbd78
Cal <callum.adair@outlook.com>
2022-07-24 23:10:29 +0100

    Added slight margin to the inventory navlink.

--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -67,7 +67,7 @@ const logout = () => {
                                     <InventoryDropdown>
                                         <template #trigger>
                                             <div type="button"
-                                                 class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition">
+                                                 class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition m-2">
                                                 <img :src="BoxIso" alt="Inventory"/> &nbsp Inventory
 
                                                 <svg
