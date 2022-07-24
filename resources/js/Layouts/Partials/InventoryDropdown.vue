c863eb493d0e9844e9e1161c1d0b21d7733261a5
Cal <callum.adair@outlook.com>
2022-07-24 15:05:18 +0100

    Created a basic inventory dropdown.

--- resources/js/Layouts/Partials/InventoryDropdown.vue
+++ resources/js/Layouts/Partials/InventoryDropdown.vue
@@ -19,27 +19,6 @@ const closeOnEscape = (e) => {
         open.value = false;
     }
 };
-
-onMounted(() => document.addEventListener('keydown', closeOnEscape));
-onUnmounted(() => document.removeEventListener('keydown', closeOnEscape));
-
-const widthClass = computed(() => {
-    return {
-        '48': 'w-48',
-    }[props.width.toString()];
-});
-
-const alignmentClasses = computed(() => {
-    if (props.align === 'left') {
-        return 'origin-top-left left-0';
-    }
-
-    if (props.align === 'right') {
-        return 'origin-top-right right-0';
-    }
-
-    return 'origin-top';
-});
 </script>
 
 <template>
