38289010bcc35f5573671cdca79bae1c47c46753
Cal <callum.adair@outlook.com>
2022-07-24 14:23:03 +0100

    Added basic skeleton of item create vue.

---
+++ resources/js/Pages/Items/Create.vue
@@ -0,0 +1,13 @@
+<script setup>
+import AppLayout from '@/Layouts/AppLayout.vue';
+</script>
+
+<template>
+    <AppLayout title="Create a new item">
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                Add an item
+            </h2>
+        </template>
+    </AppLayout>
+</template>
