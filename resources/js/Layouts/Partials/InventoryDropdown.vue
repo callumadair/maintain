a56056feb156b47a18cbae171355b471046cefe4
Cal <callum.adair@outlook.com>
2022-07-24 23:18:27 +0100

    Removed unnecessary carry overs from the jetstream dropdown.

--- resources/js/Layouts/Partials/InventoryDropdown.vue
+++ resources/js/Layouts/Partials/InventoryDropdown.vue
@@ -36,11 +36,10 @@ onUnmounted(() => document.removeEventListener('keydown', closeOnEscape));
             <div
                 v-show="open"
                 class="absolute z-50 mt-2 rounded-md shadow-lg"
-                :class="[widthClass, alignmentClasses]"
                 style="display: none;"
                 @click="open = false"
             >
-                <div class="rounded-md ring-1 ring-black ring-opacity-5" :class="contentClasses">
+                <div class="rounded-md ring-1 ring-black ring-opacity-5">
                     <slot name="content" />
                 </div>
             </div>
