802627a5ec2ca34b512d5fbc81951c62926161cc
Cal <callum.adair@outlook.com>
2022-07-27 22:51:19 +0100

    Added a basic implementation of the item create vue component.

--- resources/js/Pages/Items/Create.vue
+++ resources/js/Pages/Items/Create.vue
@@ -1,5 +1,9 @@
 <script setup>
 import AppLayout from '@/Layouts/AppLayout.vue';
+
+defineProps({
+    user: Object,
+});
 </script>
 
 <template>
@@ -9,5 +13,34 @@ import AppLayout from '@/Layouts/AppLayout.vue';
                 Add an item
             </h2>
         </template>
+
+            <form method="POST" :action="route('items.store')" class="">
+
+                <div>
+                    <label>
+                        Name
+                    </label>
+                    <input>
+                </div>
+
+
+                <div>
+                    <label>
+                        Description
+                    </label>
+                    <textarea></textarea>
+                </div>
+
+                <div>
+                    <label>
+                        Image
+                    </label>
+                    <input name="image" type="file" accept="image/png, image/jpeg, image/jpg">
+                </div>
+
+                <input name="user_id" type="hidden" value="{{user.id}}">
+
+                <input type="submit" value="Create Post">
+            </form>
     </AppLayout>
 </template>
