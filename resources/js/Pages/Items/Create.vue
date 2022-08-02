0741f4f99967578efd64c33616b4d1ffddb0df6a
Cal <callum.adair@outlook.com>
2022-08-02 23:47:49 +0100

    Removed unnecessary input tag and accessed the page prop directly.

--- resources/js/Pages/Items/Create.vue
+++ resources/js/Pages/Items/Create.vue
@@ -1,20 +1,20 @@
 <script setup>
 import AppLayout from '@/Layouts/AppLayout.vue';
-import {reactive} from 'vue';
-import {Inertia} from '@inertiajs/inertia';
-
-const form = reactive({
-    item_name: null,
-    item_description: null,
-})
-
-function submit() {
-    Inertia.post('/items/store', form)
-}
+import {useForm} from '@inertiajs/inertia-vue3';
 
 defineProps({
     user: Object,
 });
+
+const form = useForm({
+    item_name: null,
+    item_description: null,
+    user_id: props.user.id,
+})
+
+function submit() {
+    form.post(route('items.store'));
+}
 </script>
 
 <template>
@@ -25,13 +25,13 @@ defineProps({
             </h2>
         </template>
 
-        <form @submit.prevent="submit" method="POST" :action="route('items.store')"
+        <form @submit.prevent="submit"
               class="grid grid-cols-1 space-x-2 space-y-6 p-3 m-3 place-items-center border-solid border-8 border-black">
             <div class="grid justify-center place-items-center space-x-2">
                 <label for="item_name">
                     Name
                 </label>
-                <input id="item_name" v-model="form.item_name">
+                <input type="text" v-model="form.item_name">
             </div>
 
 
@@ -49,12 +49,8 @@ defineProps({
                 <input id="item_images" name="image" type="file" accept="image/png, image/jpeg, image/jpg" multiple>
             </div>
 
-            <!--            <div class="grid justify-center place-items-center space-x-2">-->
-            <!--                <input name="user_id" type="hidden" value="{{user.id}}" v-model="form.user_id"/>-->
-            <!--            </div>-->
-
             <div class="justify-center place-items-center space-x-2 hover:bg-white">
-                <button type="submit">Create Item</button>
+                <button type="submit" :disabled="form.processing">Create Item</button>
             </div>
         </form>
     </AppLayout>
