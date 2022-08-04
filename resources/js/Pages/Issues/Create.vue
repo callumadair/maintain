3917f284be78ba0040af669b241bcc810a10a908
Cal <callum.adair@outlook.com>
2022-08-04 13:58:27 +0100

    Implemented an issue create component based off the item create component.

---
+++ resources/js/Pages/Issues/Create.vue
@@ -0,0 +1,57 @@
+<script setup>
+import AppLayout from '@/Layouts/AppLayout.vue';
+import { useForm, usePage } from '@inertiajs/inertia-vue3';
+
+defineProps({
+    user: Object,
+});
+
+const form = useForm({
+    issue_title: null,
+    issue_description: null,
+    user_id: usePage().props.value.user.id,
+})
+
+function submit() {
+    form.post(route('issues.store'));
+}
+</script>
+
+<template>
+    <AppLayout title="Add a new item">
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                Add an issue
+            </h2>
+        </template>
+
+        <form @submit.prevent="submit"
+              class="grid grid-cols-1 space-x-2 space-y-6 p-3 m-3 place-items-center border-solid border-8 border-black">
+            <div class="grid justify-center place-items-center space-x-2">
+                <label for="item_name">
+                    Title
+                </label>
+                <input type="text" v-model="form.issue_title">
+            </div>
+
+
+            <div class="grid justify-center place-items-center space-x-2">
+                <label for="item_description">
+                    Description
+                </label>
+                <textarea id="item_description" v-model="form.issue_description"/>
+            </div>
+
+            <div class="grid justify-center place-items-center space-x-2">
+                <label for="item_images">
+                    Images
+                </label>
+                <input id="item_images" name="image" type="file" accept="image/png, image/jpeg, image/jpg" multiple>
+            </div>
+
+            <div class="justify-center place-items-center space-x-2 hover:bg-white">
+                <button type="submit" :disabled="form.processing">Create Issue</button>
+            </div>
+        </form>
+    </AppLayout>
+</template>
