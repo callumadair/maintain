e5e05741051305564b9f497785a5b07d81fabdad
Cal <callum.adair@outlook.com>
2022-08-06 23:09:10 +0100

    Formatting.

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -75,10 +75,10 @@ class ItemController extends Controller
                 $new_image = new Image;
                 $new_image->name = $item_image->getClientOriginalName();
                 $new_image->item_id = $item->id;
-
-                Storage::disk('public')->put('/images', $item_image);
                 $new_image->image_path = 'storage/images/' . $item_image->hashName();
                 $new_image->save();
+
+                Storage::disk('public')->put('/images', $item_image);
             }
         }
 
