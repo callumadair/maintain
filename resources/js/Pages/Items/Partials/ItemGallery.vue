c2653d8aebefa5d2412ed94889e3787233679e5e
Cal <callum.adair@outlook.com>
2022-08-10 22:37:57 +0100

    Initial implementation of the item gallery in the style of the existing description.

--- resources/js/Pages/Items/Partials/ItemGallery.vue
+++ resources/js/Pages/Items/Partials/ItemGallery.vue
@@ -1,9 +1,17 @@
-<template>
-
-</template>
-
 <script setup>
 defineProps({
     images: Object,
 });
 </script>
+
+<template>
+    <div>
+        <div class="my-2 flex flex-row justify-center items-center bg-white overflow-hidden shadow-xl sm:rounded-lg">
+            <div class="w-48 m-6"
+                 v-for="image in images">
+                <img :src="image['image_path']"
+                     alt="An image of an item">
+            </div>
+        </div>
+    </div>
+</template>
