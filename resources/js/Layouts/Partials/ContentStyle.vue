de8c96b5e6037fc5de6c78c776c5a5016a910481
Cal <callum.adair@outlook.com>
2022-08-10 23:26:00 +0100

    Created the content style component for handling content of the pages.

---
+++ resources/js/Layouts/Partials/ContentStyle.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="py-6">
+        <div class="max-w-7xl mx-auto px-6">
+            <div class="p-2 bg-white overflow-hidden shadow-xl rounded-lg">
+                <slot name="content">
+
+                </slot>
+            </div>
+        </div>
+    </div>
+</template>
