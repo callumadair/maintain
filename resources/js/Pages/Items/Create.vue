1cf55fc87c2a4c0a9d423d3b8682d369cf964747
Cal <callum.adair@outlook.com>
2022-08-11 22:15:04 +0100

    Moved the create form into a content style component and styled the create item button.

--- resources/js/Pages/Items/Create.vue
+++ resources/js/Pages/Items/Create.vue
@@ -1,5 +1,6 @@
 <script setup>
 import AppLayout from '@/Layouts/AppLayout.vue';
+import ContentStyle from '@/Layouts/Partials/ContentStyle.vue';
 import {useForm, usePage} from '@inertiajs/inertia-vue3';
 
 defineProps({
@@ -26,34 +27,45 @@ function submit() {
             </h2>
         </template>
 
-        <form @submit.prevent="submit"
-              class="grid grid-cols-1 space-x-2 space-y-6 p-3 m-3 place-items-center border-solid border-8 border-black">
-            <div class="grid justify-center place-items-center space-x-2">
-                <label for="item_name">
-                    Name
-                </label>
-                <input type="text" v-model="form.item_name">
-            </div>
+        <ContentStyle>
+            <template #content>
+                <form @submit.prevent="submit"
+                      class="grid grid-cols-1 space-x-2 space-y-6 p-3 m-3 place-items-center">
+                    <div class="grid justify-center place-items-center space-x-2">
+                        <label for="item_name">
+                            Name
+                        </label>
+                        <input type="text" v-model="form.item_name">
+                    </div>
 
 
-            <div class="grid justify-center place-items-center space-x-2">
-                <label for="item_description">
-                    Description
-                </label>
-                <textarea id="item_description" v-model="form.item_description"/>
-            </div>
+                    <div class="grid justify-center place-items-center space-x-2">
+                        <label for="item_description">
+                            Description
+                        </label>
+                        <textarea id="item_description" v-model="form.item_description"/>
+                    </div>
 
-            <div class="grid justify-center place-items-center space-x-2">
-                <label for="item_images">
-                    Images
-                </label>
-                <input id="item_images" name="image" type="file" accept="image/png, image/jpeg, image/jpg" multiple
-                       @input="form.item_images = $event.target.files"/>
-            </div>
+                    <div class="grid justify-center place-items-center space-x-2">
+                        <label for="item_images">
+                            Images
+                        </label>
+                        <input id="item_images" name="image" type="file" accept="image/png, image/jpeg, image/jpg"
+                               multiple
+                               @input="form.item_images = $event.target.files"/>
+                    </div>
 
-            <div class="justify-center place-items-center space-x-2 hover:bg-white">
-                <button type="submit" :disabled="form.processing">Create Item</button>
-            </div>
-        </form>
+
+                    <div class="py-6">
+                        <button type="submit" :disabled="form.processing">
+                        <span class="m-2 p-4 space-x-2 hover:bg-gray-50 hover:text-indigo-400 rounded-lg">
+                            Create Item
+                        </span>
+                        </button>
+                    </div>
+
+                </form>
+            </template>
+        </ContentStyle>
     </AppLayout>
 </template>
