efbcb4dae12f501d76eedb88c81b5f14966ec6ee
Cal <callum.adair@outlook.com>
2022-08-11 13:28:13 +0100

    Formatting

--- resources/js/Layouts/Partials/ContentStyle.vue
+++ resources/js/Layouts/Partials/ContentStyle.vue
@@ -2,9 +2,7 @@
     <div class="py-6">
         <div class="max-w-7xl mx-auto px-6">
             <div class="p-2 bg-white overflow-hidden shadow-xl rounded-lg">
-                <slot name="content">
-
-                </slot>
+                <slot name="content"/>
             </div>
         </div>
     </div>
