592fee9e4f51b7ed5cd87137ceb08a9ebf12e96e
Cal <callum.adair@outlook.com>
2022-08-14 15:50:04 +0100

    Moved the issue edit component into a content style component.

--- resources/js/Pages/Issues/Edit.vue
+++ resources/js/Pages/Issues/Edit.vue
@@ -1,5 +1,6 @@
 <script setup>
 import AppLayout from '@/Layouts/AppLayout.vue';
+import ContentStyle from '@/Layouts/Partials/ContentStyle.vue';
 import {useForm, usePage} from '@inertiajs/inertia-vue3';
 
 defineProps({
@@ -27,34 +28,38 @@ function submit() {
             </h2>
         </template>
 
-        <form @submit.prevent="submit"
-              class="grid grid-cols-1 space-x-2 space-y-6 p-3 m-3 place-items-center border-solid border-8 border-black">
-            <div class="grid justify-center place-items-center space-x-2">
-                <label for="issue_title">
-                    Title
-                </label>
-                <input id="issue_title" type="text" v-model="form.issue_title">
-            </div>
+        <ContentStyle>
+            <template #content>
+                <form @submit.prevent="submit"
+                      class="grid grid-cols-1 space-x-2 space-y-6 p-3 m-3 place-items-center">
+                    <div class="grid justify-center place-items-center space-x-2">
+                        <label for="issue_title">
+                            Title
+                        </label>
+                        <input id="issue_title" type="text" v-model="form.issue_title">
+                    </div>
 
 
-            <div class="grid justify-center place-items-center space-x-2">
-                <label for="issue_description">
-                    Description
-                </label>
-                <textarea id="issue_description" v-model="form.issue_description">
-                </textarea>
-            </div>
+                    <div class="grid justify-center place-items-center space-x-2">
+                        <label for="issue_description">
+                            Description
+                        </label>
+                        <textarea id="issue_description" v-model="form.issue_description"/>
+                    </div>
 
-            <div class="grid justify-center place-items-center space-x-2">
-                <label for="issue_images">
-                    Images
-                </label>
-                <input id="issue_images" name="image" type="file" accept="image/png, image/jpeg, image/jpg" multiple>
-            </div>
+                    <div class="grid justify-center place-items-center space-x-2">
+                        <label for="issue_images">
+                            Images
+                        </label>
+                        <input id="issue_images" name="image" type="file" accept="image/png, image/jpeg, image/jpg"
+                               multiple>
+                    </div>
 
-            <div class="justify-center place-items-center space-x-2 hover:bg-white">
-                <button type="submit" :disabled="form.processing">Update Issue</button>
-            </div>
-        </form>
+                    <div class="justify-center place-items-center space-x-2 hover:bg-white">
+                        <button type="submit" :disabled="form.processing">Update Issue</button>
+                    </div>
+                </form>
+            </template>
+        </ContentStyle>
     </AppLayout>
 </template>
