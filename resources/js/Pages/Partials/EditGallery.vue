74bd8055a31bed4bc1f30dd799b9f17a7b9a3b86
Cal <callum.adair@outlook.com>
2022-08-14 22:55:06 +0100

    Formatting.

--- resources/js/Pages/Partials/EditGallery.vue
+++ resources/js/Pages/Partials/EditGallery.vue
@@ -9,8 +9,8 @@ defineProps({
 <template>
     <div>
         <div class="my-2 flex flex-row flex-wrap justify-center items-center bg-white shadow-xl rounded-lg mx-6">
-            <div class="w-48 m-6"
-                 v-for="image in images">
+            <div v-for="image in images"
+                 class="w-48 m-6">
                 <img :src="image['image_path']"
                      alt="An image of an item">
                 <button>
