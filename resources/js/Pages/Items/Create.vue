50e71213b92977cf94643a7b5437602209eaf10f
Cal <callum.adair@outlook.com>
2022-08-14 22:53:59 +0100

    Formatting.

--- resources/js/Pages/Items/Create.vue
+++ resources/js/Pages/Items/Create.vue
@@ -29,13 +29,14 @@ function submit() {
 
         <ContentStyle>
             <template #content>
-                <form @submit.prevent="submit"
-                      class="grid grid-cols-1 space-x-2 space-y-6 p-3 m-3 place-items-center">
+                <form class="grid grid-cols-1 space-x-2 space-y-6 p-3 m-3 place-items-center"
+                      @submit.prevent="submit">
                     <div class="grid justify-center place-items-center space-x-2">
                         <label for="item_name">
                             Name
                         </label>
-                        <input type="text" v-model="form.item_name">
+                        <input v-model="form.item_name"
+                               type="text">
                     </div>
 
 
@@ -43,21 +44,26 @@ function submit() {
                         <label for="item_description">
                             Description
                         </label>
-                        <textarea id="item_description" v-model="form.item_description"/>
+                        <textarea id="item_description"
+                                  v-model="form.item_description"/>
                     </div>
 
                     <div class="grid justify-center place-items-center space-x-2">
                         <label for="item_images">
                             Images
                         </label>
-                        <input id="item_images" name="image" type="file" accept="image/png, image/jpeg, image/jpg"
+                        <input id="item_images"
+                               accept="image/png, image/jpeg, image/jpg"
                                multiple
+                               name="image"
+                               type="file"
                                @input="form.item_images = $event.target.files"/>
                     </div>
 
 
                     <div class="py-6">
-                        <button type="submit" :disabled="form.processing">
+                        <button :disabled="form.processing"
+                                type="submit">
                         <span class="m-2 p-4 space-x-2 hover:bg-gray-50 hover:text-indigo-400 rounded-lg">
                             Create Item
                         </span>
