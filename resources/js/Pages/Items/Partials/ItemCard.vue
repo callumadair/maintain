7686295fb600b75e10706f3140e21d156e843ca8
Cal <callum.adair@outlook.com>
2022-08-14 22:56:54 +0100

    Formatting.

--- resources/js/Pages/Items/Partials/ItemCard.vue
+++ resources/js/Pages/Items/Partials/ItemCard.vue
@@ -8,16 +8,16 @@ defineProps({
 
 <template>
     <div>
-        <Link :href="route('items.show', item.id)" :data="item">
+        <Link :data="item"
+              :href="route('items.show', item.id)">
             <div class="grid text-center place-items-center p-3 hover:bg-gray-50 hover:text-indigo-400">
                 {{ item.name }}
-
-                <div v-if="item.images.length > 0" class="w-48 place-items-center">
+                <div v-if="item.images.length > 0"
+                     class="w-48 place-items-center">
                     <img :src="item.images[0]['image_path']"
                          alt="First image linked to this item, if it has any.">
                 </div>
             </div>
-
         </Link>
     </div>
 </template>
