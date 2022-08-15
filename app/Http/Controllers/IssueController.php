c657d087fadac907e2ad84005d7c5dac8cb08d85
Cal <callum.adair@outlook.com>
2022-08-15 22:23:29 +0100

    Added image storing to the issue controller.

--- app/Http/Controllers/IssueController.php
+++ app/Http/Controllers/IssueController.php
@@ -2,16 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Image;
 use App\Models\Issue;
 use App\Models\Item;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
 use Inertia\Inertia;
 use Inertia\Response;
 
 class IssueController extends Controller
 {
+    const IMAGES_ROOT = '/storage/images/issues';
+
     /**
      * Display a listing of the resource.
      *
@@ -57,6 +61,7 @@ class IssueController extends Controller
         $validated_data = $request->validate([
             'issue_title' => 'required',
             'issue_description' => 'required',
+            'issue_images' => 'nullable|image|mimes:jpeg,png,jpg',
             'item_id' => 'required',
             'originator_id' => 'required|numeric',
         ]);
@@ -69,6 +74,28 @@ class IssueController extends Controller
         //temporarily set the assignee id to just be the originator id.
         $issue->assignee_id = $validated_data['originator_id'];
         $issue->save();
+
+        if ($request->hasFile('issue_images')) {
+            $issue_images = $request->file('item_images');
+            $image_path_prefix = self::IMAGES_ROOT
+                . $issue->id . '-'
+                . $issue->title . '-';
+
+            foreach ($issue_images as $item_image) {
+                $new_image = new Image;
+                $new_image->name = $item_image->getClientOriginalName();
+                $new_image->issue_id = $issue->id;
+                $new_image->image_path = $image_path_prefix
+                    . $item_image->getClientOriginalName();
+                $new_image->save();
+
+                Storage::disk('public')->putFileAs('/images',
+                    $item_image
+                    , $issue->id
+                    . '-' . $issue->title
+                    . '-' . $new_image->name);
+            }
+        }
 
         return redirect()->route('issues.show', ['issue' => $issue]);
     }
