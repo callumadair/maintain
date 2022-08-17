afe26cc13f740422aa8bf3532b349dd8a0c573a8
Cal <callum.adair@outlook.com>
2022-08-17 22:48:32 +0100

    Moved the issue index content into a content style component.

--- resources/js/Pages/Issues/Index.vue
+++ resources/js/Pages/Issues/Index.vue
@@ -1,5 +1,6 @@
 <script setup>
 import AppLayout from '@/Layouts/AppLayout.vue';
+import ContentStyle from '@/Layouts/Partials/ContentStyle.vue';
 import IssueCard from '@/Pages/Issues/Partials/IssueCard.vue';
 
 defineProps({
@@ -15,16 +16,14 @@ defineProps({
             </h2>
         </template>
 
-        <div class="py-12">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                    <ul>
-                        <li v-for="issue in issues">
-                            <IssueCard :issue="issue"/>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </div>
+        <ContentStyle>
+            <template #content>
+                <ul>
+                    <li v-for="issue in issues">
+                        <IssueCard :issue="issue"/>
+                    </li>
+                </ul>
+            </template>
+        </ContentStyle>
     </AppLayout>
 </template>
