a7a34711ce18f5edbae082bc6cca6990a29552e0
Cal <callum.adair@outlook.com>
2022-08-18 16:11:34 +0100

    Added the display of the first image of the issue if it has any.

--- resources/js/Pages/Issues/Partials/IssueCard.vue
+++ resources/js/Pages/Issues/Partials/IssueCard.vue
@@ -10,8 +10,13 @@ defineProps({
     <div>
         <Link :data="issue"
               :href="route('issues.show', issue.id)">
-            <div class="text-center p-3 hover:bg-gray-50 hover:text-indigo-400">
+            <div class="grid text-center place-items-center p-3 hover:bg-gray-50 hover:text-indigo-400">
                 {{ issue.title }}
+                <div v-if="issue.images.length > 0"
+                     class="w-48 place-items-center">
+                    <img :src="issue.images[0]['image_path']"
+                         alt="First image linked to this issue, if it has any.">
+                </div>
             </div>
         </Link>
     </div>
