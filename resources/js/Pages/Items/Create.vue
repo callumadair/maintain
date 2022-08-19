347a62bce86977494977a2d7020ced43efb37ae0
Cal <callum.adair@outlook.com>
2022-08-19 23:28:00 +0100

    Styled the item create component to be consistent with the styling of the item edit component.

--- resources/js/Pages/Items/Create.vue
+++ resources/js/Pages/Items/Create.vue
@@ -30,21 +30,25 @@ const submit = () => form.post(route('items.store'));
                 <form class="grid grid-cols-1 space-x-2 space-y-6 p-3 m-3 place-items-center"
                       @submit.prevent="submit">
                     <div class="grid justify-center place-items-center space-x-2">
-                        <label for="item_name">
+                        <label class="text-lg"
+                               for="item_name">
                             Name
                         </label>
                         <input id="item_name"
-                            v-model="form.item_name"
+                               v-model="form.item_name"
+                               class="m-3 rounded-lg text-center border-transparent bg-gray-50"
                                type="text">
                     </div>
 
 
                     <div class="grid justify-center place-items-center space-x-2">
-                        <label for="item_description">
+                        <label class="text-lg"
+                               for="item_description">
                             Description
                         </label>
                         <textarea id="item_description"
-                                  v-model="form.item_description"/>
+                                  v-model="form.item_description"
+                                  class="resize-none m-3 rounded-lg text-center border-transparent w-72 h-36 bg-gray-50"/>
                     </div>
 
                     <div class="grid justify-center place-items-center space-x-2">
@@ -53,10 +57,12 @@ const submit = () => form.post(route('items.store'));
                         </label>
                         <input id="item_images"
                                accept="image/png, image/jpeg, image/jpg"
+                               class="file:rounded-lg file:p-2 file:bg-white file:border-transparent hover:file:text-indigo-400 hover:file:bg-gray-50 p-2 border-solid border-2 rounded-lg border-gray-300"
                                multiple
                                name="image"
                                type="file"
-                               @input="form.item_images = $event.target.files"/>
+                               @input="form.item_images = $event.target.files"
+                        />
                     </div>
 
 
