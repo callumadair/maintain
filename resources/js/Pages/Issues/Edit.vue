dcb2a70fc1afe30771fee1293bc20e7f39cd893a
Cal <callum.adair@outlook.com>
2022-08-20 00:11:33 +0100

    Styled the issue edit component in the style of the item edit and create components.

--- resources/js/Pages/Issues/Edit.vue
+++ resources/js/Pages/Issues/Edit.vue
@@ -36,21 +36,25 @@ const submit = () => form.post(route('issues.update', usePage().props.value.issu
                 <form class="grid grid-cols-1 space-x-2 space-y-6 p-3 m-3 place-items-center"
                       @submit.prevent="submit">
                     <div class="grid justify-center place-items-center space-x-2">
-                        <label for="issue_title">
+                        <label class="text-lg"
+                               for="issue_title">
                             Title
                         </label>
                         <input id="issue_title"
                                v-model="form.issue_title"
+                               class="m-3 rounded-lg text-center border-transparent bg-gray-50"
                                type="text">
                     </div>
 
 
                     <div class="grid justify-center place-items-center space-x-2">
-                        <label for="issue_description">
+                        <label class="text-lg"
+                               for="issue_description">
                             Description
                         </label>
                         <textarea id="issue_description"
-                                  v-model="form.issue_description"/>
+                                  v-model="form.issue_description"
+                                  class="resize-none m-3 rounded-lg text-center border-transparent w-72 h-36 bg-gray-50"/>
                     </div>
 
                     <div v-if="issue.images.length > 0">
@@ -58,11 +62,13 @@ const submit = () => form.post(route('issues.update', usePage().props.value.issu
                     </div>
 
                     <div class="grid justify-center place-items-center space-x-2">
-                        <label for="issue_images">
+                        <label class="text-lg"
+                               for="issue_images">
                             Images
                         </label>
                         <input id="issue_images"
                                accept="image/png, image/jpeg, image/jpg"
+                               class="file:rounded-lg file:p-2 file:bg-white file:border-transparent hover:file:text-indigo-400 hover:file:bg-gray-50 p-2 border-solid border-2 rounded-lg border-gray-300"
                                multiple
                                name="image"
                                type="file"
