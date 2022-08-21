58e6c39f197b9135d04dcefaed3462642a86148c
Cal <callum.adair@outlook.com>
2022-08-21 15:56:40 +0100

    Implemented deleting the image file when the image model is deleted not in a mass delete.

--- app/Models/Image.php
+++ app/Models/Image.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Support\Facades\Storage;
 
 /**
  * @property string $name
@@ -24,5 +25,16 @@ class Image extends Model
     public function issue(): BelongsTo
     {
         return $this->belongsTo(Issue::class);
+    }
+
+    protected static function booted()
+    {
+        parent::booted();
+
+        static::deleted(function ($image) {
+            //remove storage/ form the path
+            $relative_path = substr($image->image_path, 8);
+            Storage::disk('public')->delete($relative_path);
+        });
     }
 }
