af902391730ca1fcd7fd5739a4e307ec5aef2ad9
Cal <callum.adair@outlook.com>
2022-08-21 17:43:11 +0100

    Styling of the edit gallery.

--- resources/js/Pages/Components/EditGallery.vue
+++ resources/js/Pages/Components/EditGallery.vue
@@ -20,18 +20,18 @@ const greyOut = (image_id) => {
 
 <template>
     <div>
-        <div class="my-2 flex flex-row flex-wrap justify-center items-center bg-white shadow-xl rounded-lg mx-6">
+        <div class="my-2 flex flex-row flex-wrap justify-center items-center bg-white shadow-xl rounded-lg mx-6 w-full">
             <div v-for="image in images"
                  class="w-48 m-6">
-                <img :id="'image' + image.id"
-                     :src="image['image_path']"
-                     alt="An image of an item"
-                     class="">
                 <button type="button"
                         @click="greyOut(image.id);
                         $emit('imagesChanged', image.id)">
                     <img :src="RemoveMediaImage" alt="Delete square.">
                 </button>
+                <img :id="'image' + image.id"
+                     :src="image['image_path']"
+                     alt="An image of an item"/>
+
             </div>
         </div>
     </div>
