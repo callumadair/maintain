17289b7dd6bce034dc800a0d39f89d74999a35b9
Cal <31387289+callumadair@users.noreply.github.com>
2022-08-28 00:12:34 +0100

    Revert "Revert "Created a vite.config.ts file and based it off the example given at https://tech.codivores.com/ltivt-2-inertia-vite-vue3-typescript""
    This reverts commit 3ac89d06a9b16d89a7603f017959b92df060d6f1.

---
+++ vite.config.ts
@@ -0,0 +1,33 @@
+import {defineConfig} from 'vite';
+import laravel from 'laravel-vite-plugin';
+import vue from '@vitejs/plugin-vue';
+
+export default defineConfig({
+    server: {
+        hmr: {
+            host: '127.0.0.1',
+        },
+    },
+    plugins: [
+        laravel({
+            input: 'resources/js/app.js',
+            refresh: true,
+        }),
+        vue({
+            template: {
+                transformAssetUrls: {
+                    base: null,
+                    includeAbsolute: false,
+                },
+            },
+        }),
+    ],
+    optimizeDeps: {
+        include: [
+            "@inertiajs/inertia",
+            "@inertiajs/inertia-vue3",
+            "axios",
+            "vue"
+        ],
+    },
+});
