88e33b7a9bcd37c3a1f6132af8d36f130010b48c
Cal <31387289+callumadair@users.noreply.github.com>
2022-08-28 00:11:53 +0100

    Revert "Converted the edit gallery component to typescript."
    This reverts commit df1fc8d60b9ce3a95da412a8eb4b088f8c52acae.

--- resources/js/Pages/Components/EditGallery.vue
+++ resources/js/Pages/Components/EditGallery.vue
@@ -1,4 +1,4 @@
-<script lang="ts" setup>
+<script setup>
 import RemoveMediaImage from 'iconoir/icons/remove-media-image.svg';
 
 defineProps({
@@ -7,8 +7,8 @@ defineProps({
 
 const emit = defineEmits(['imagesChanged']);
 
-const greyOut = (image_id: number) => {
-    let image_element: HTMLImageElement = document.getElementById("image" + image_id) as HTMLImageElement;
+const greyOut = (image_id) => {
+    let image_element = document.getElementById("image" + image_id);
 
     if (image_element.classList.contains("grayscale")) {
         image_element.classList.remove("grayscale");
