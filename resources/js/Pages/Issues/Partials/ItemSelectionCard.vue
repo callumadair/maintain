02c296a3b0867707f70712e8988300709d20e3b1
Cal <callum.adair@outlook.com>
2022-09-02 00:57:38 +0100

    Added an initial item selection card based off the item card component.

---
+++ resources/js/Pages/Issues/Partials/ItemSelectionCard.vue
@@ -0,0 +1,27 @@
+<script lang="ts" setup>
+import {Link} from "@inertiajs/inertia-vue3";
+import {PropType} from "vue";
+import Item = App.Models.Item;
+
+const props = defineProps({
+    item: {
+        type: Object as PropType<Item>,
+        required: true,
+    },
+});
+</script>
+
+<template>
+    <div class="space-y-2">
+        <Link :href="route('issues.create', item.id)">
+            <div class="grid p-3 text-center place-items-center rounded-lg hover:bg-gray-50 hover:text-indigo-400">
+                {{ item.name }}
+                <div v-if="item.images.length > 0"
+                     class="w-48 place-items-center">
+                    <img :src="item.images[0]['image_path']"
+                         alt="First image linked to this item, if it has any.">
+                </div>
+            </div>
+        </Link>
+    </div>
+</template>
