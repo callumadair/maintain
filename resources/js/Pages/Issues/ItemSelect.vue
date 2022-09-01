3bc67968ebe0ecd74c3c1dc8ea8abf01cd7e1605
Cal <callum.adair@outlook.com>
2022-09-01 23:38:57 +0100

    Created an initial item select component based off the item index component.

---
+++ resources/js/Pages/Issues/ItemSelect.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import AppLayout from '@/Layouts/AppLayout.vue';
+import ContentStyle from '@/Pages/Components/ContentStyle.vue';
+
+import {PropType} from "vue";
+import Item = App.Models.Item;
+
+const props = defineProps({
+    items: {
+        type: Array as PropType<Array<Item>>,
+        required: true,
+    },
+});
+</script>
+
+<template>
+    <AppLayout title="Item Selection Menu">
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                Select an Item
+            </h2>
+        </template>
+
+        <ContentStyle>
+            <template #content>
+                <ul>
+                    <li v-for="item in items">
+                        {{ item.name}}
+                    </li>
+                </ul>
+            </template>
+        </ContentStyle>
+    </AppLayout>
+</template>
