c2b1208c85623d63213dd8dae07058efebd7471f
Cal <callum.adair@outlook.com>
2022-09-04 23:26:27 +0100

    Implemented a user card with some styling and emitting an event.

---
+++ resources/js/Pages/Users/Partials/UserCard.vue
@@ -0,0 +1,20 @@
+<script lang="ts" setup>
+import {PropType} from "vue";
+import User = App.Models.User;
+
+const props = defineProps({
+    user: {
+        type: Object as PropType<User>,
+        required: true,
+    }
+});
+</script>
+
+<template>
+    <div class="px-4 py-2 rounded-lg text-center hover:bg-gray-50 hover:text-indigo-400">
+        <button type="button"
+                @click="$emit(user.id)">
+            {{ user.name }}
+        </button>
+    </div>
+</template>
