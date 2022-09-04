380f81049e6980d347d20fb817e27e59a04a17e9
Cal <callum.adair@outlook.com>
2022-09-04 15:03:40 +0100

    Adjusted the styling for the issue edit gallery.

--- resources/js/Pages/Issues/Edit.vue
+++ resources/js/Pages/Issues/Edit.vue
@@ -64,7 +64,7 @@ const handleImagesChanged = (image_id: number) => {
                 <form class="grid grid-cols-1 space-y-6 p-3 m-3` justify-center place-items-center"
                       @submit.prevent="submit">
 
-                    <div class="grid space-y-2 justify-center place-items-center">
+                    <div class="grid space-y-3 justify-center place-items-center">
                         <label class="text-lg"
                                for="issue_title">
                             Title
@@ -76,7 +76,7 @@ const handleImagesChanged = (image_id: number) => {
                     </div>
 
 
-                    <div class="grid space-y-2 justify-center place-items-center">
+                    <div class="grid space-y-3 justify-center place-items-center">
                         <label class="text-lg"
                                for="issue_description">
                             Description
@@ -87,7 +87,7 @@ const handleImagesChanged = (image_id: number) => {
                     </div>
 
                     <div v-if="issue.images.length > 0"
-                         class="grid space-y-2 justify-center place-items-center text-lg">
+                         class="grid space-y-3 justify-center place-items-center text-lg">
                         <label for="edit_gallery">
                             Remove Images
                         </label>
@@ -96,7 +96,7 @@ const handleImagesChanged = (image_id: number) => {
                                      @images-changed="handleImagesChanged"/>
                     </div>
 
-                    <div class="grid space-y-2 justify-center place-items-center">
+                    <div class="grid space-y-3 justify-center place-items-center">
                         <label class="text-lg"
                                for="issue_images">
                             Images
@@ -110,7 +110,7 @@ const handleImagesChanged = (image_id: number) => {
                                @input="form.issue_images = $event.target.files">
                     </div>
 
-                    <div class="space-y-2 justify-center hover:bg-white">
+                    <div class="space-y-3 justify-center hover:bg-white">
                         <button :disabled="form.processing"
                                 type="submit">
                             <span class="p-4 rounded-lg hover:bg-gray-50 hover:text-indigo-400">
