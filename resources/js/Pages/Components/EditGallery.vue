16dc61b74aafaa14043f7baa39c69f6914731177
Cal <callum.adair@outlook.com>
2022-09-04 23:46:06 +0100

    Switched to using classlist.toggle and improved formatting.

--- resources/js/Pages/Components/EditGallery.vue
+++ resources/js/Pages/Components/EditGallery.vue
@@ -14,29 +14,24 @@ const emit = defineEmits(['imagesChanged']);
 
 const greyOut = (image_id: number) => {
     let image_element: HTMLImageElement = document.getElementById("image" + image_id) as HTMLImageElement;
-
-    if (image_element.classList.contains("grayscale")) {
-        image_element.classList.remove("grayscale");
-    } else {
-        image_element.classList.add("grayscale");
-    }
+    image_element.classList.toggle("grayscale");
 }
 </script>
 
 <template>
-        <div
-            class="mx-6 my-2 flex flex-row flex-wrap justify-center items-center bg-white rounded-lg border-solid border-2 border-gray-200">
-            <div v-for="image in images"
-                 class="w-48 m-6">
-                <button type="button"
-                        @click="greyOut(image.id);
+    <div class="mx-6 my-2 flex flex-row flex-wrap justify-center items-center bg-white rounded-lg
+                border-solid border-2 border-gray-200">
+        <div v-for="image in images"
+             class="w-48 m-6">
+            <button type="button"
+                    @click="greyOut(image.id);
                         $emit('imagesChanged', image.id)">
-                    <img :src="RemoveMediaImage"
-                         alt="Delete square.">
-                </button>
-                <img :id="'image' + image.id"
-                     :src="image['image_path']"
-                     alt="An image of an item"/>
-            </div>
+                <img :src="RemoveMediaImage"
+                     alt="Delete square.">
+            </button>
+            <img :id="'image' + image.id"
+                 :src="image['image_path']"
+                 alt="An image of an item"/>
         </div>
+    </div>
 </template>
