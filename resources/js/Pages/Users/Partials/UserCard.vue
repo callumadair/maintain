f1dfc5fa7ca92785edebbeb44f7d6b2a29c4d352
Cal <callum.adair@outlook.com>
2022-09-04 23:44:46 +0100

    Added a colour change function on click.

--- resources/js/Pages/Users/Partials/UserCard.vue
+++ resources/js/Pages/Users/Partials/UserCard.vue
@@ -8,12 +8,21 @@ const props = defineProps({
         required: true,
     }
 });
+
+const greyOut = (user_id: number) => {
+    let user_element = document.getElementById("user" + user_id) as HTMLDivElement;
+    user_element.classList.toggle("bg-gray-50");
+    user_element.classList.toggle("text-indigo-400");
+}
+
 </script>
 
 <template>
-    <div class="px-4 py-2 rounded-lg text-center hover:bg-gray-50 hover:text-indigo-400">
+    <div :id="'user' + user.id"
+         class="px-4 py-2 rounded-lg text-center hover:bg-gray-50 hover:text-indigo-400">
         <button type="button"
-                @click="$emit(user.id)">
+                @click="greyOut(user.id);
+                $emit('userSelected', user.id)">
             {{ user.name }}
         </button>
     </div>
