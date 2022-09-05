b257e30a11ae7810a785f3af25f315ae86cf0879
Cal <callum.adair@outlook.com>
2022-09-06 00:05:28 +0100

    Restyled the item show component and moved everything into a content style component and had additional restructuring.

--- resources/js/Pages/Items/Show.vue
+++ resources/js/Pages/Items/Show.vue
@@ -25,71 +25,84 @@ const destroy = (id: number) => Inertia.delete(route('items.destroy', id));
             </h2>
         </template>
 
-        <ContentStyle v-if="item.description"
-                      id="item_description">
+        <ContentStyle>
             <template #content>
-                {{ item.description }}
-            </template>
-        </ContentStyle>
+                <div v-if="item.description"
+                     id="item_description"
+                     class="p-2 text-center">
+                    {{ item.description }}
+                </div>
 
-        <div v-if="item.images.length > 0"
-             id="images_gallery">
-            <Gallery :images="item.images"/>
-        </div>
+                <div v-if="item.description"
+                     class="border-[1px] border-gray-200 rounded-lg"/>
 
-        <div v-if="item.issues.length > 0"
-             class="py-2">
-            <div class="grid justify-center text-center ">
-                <div class="flex flex-col bg-white shadow-xl rounded-lg overflow-hidden w-96">
-                    <label class="m-2"
-                           for="issues_list">
-                        Issues associated with this item.
-                    </label>
-                    <div class="border-b-2 border-gray-200 rounded-lg"/>
-                    <div class="m-2 overflow-y-scroll max-h-24 scrollbar-thin scrollbar-thumb-slate-200">
+                <div v-if="item.images.length > 0"
+                     id="images_gallery">
+                    <Gallery :images="item.images"/>
+                </div>
 
-                        <ul id="issues_list">
-                            <li v-for="issue in item.issues">
-                                <Link id="issue_show_link"
-                                      :href="route('issues.show', issue.id)">
-                                    <div class="p-2 hover:bg-gray-50 hover:text-indigo-400 rounded-lg">
-                                        {{ issue.title }}
-                                    </div>
-                                </Link>
-                            </li>
-                        </ul>
+                <div v-if="item.images.length > 0"
+                     class="border-[1px] border-gray-200 rounded-lg"/>
+
+                <div v-if="item.issues.length > 0"
+                     class="py-2">
+                    <div class="grid justify-center text-center ">
+                        <div class="p-4 w-96 flex flex-col bg-white rounded-lg overflow-hidden border-2">
+                            <label class="m-2"
+                                   for="issues_list">
+                                Issues associated with this item.
+                            </label>
+
+                            <div class="border-b-2 border-gray-200 rounded-lg"/>
+
+                            <div class="m-2 overflow-y-scroll max-h-24 scrollbar-thin scrollbar-thumb-slate-200">
+                                <ul id="issues_list">
+                                    <li v-for="issue in item.issues">
+                                        <Link id="issue_show_link"
+                                              :href="route('issues.show', issue.id)">
+                                            <div class="p-2 hover:bg-gray-50 hover:text-indigo-400 rounded-lg">
+                                                {{ issue.title }}
+                                            </div>
+                                        </Link>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
                     </div>
                 </div>
-            </div>
-        </div>
 
-        <div class="py-6">
-            <div class="flex justify-center">
-                <div class="flex bg-white shadow-xl rounded-lg overflow-hidden justify-center">
-                    <Link id="add_issue_link"
-                          :href="route('issues.create', item.id)">
-                        <div class="p-4 hover:bg-gray-50 hover:text-indigo-400">
-                            Add a new issue
+                <div v-if="item.issues.length > 0"
+                     class="border-[1px] border-gray-200 rounded-lg"/>
+
+                <div class="py-6">
+                    <div class="flex justify-center">
+                        <div class="flex justify-center bg-white rounded-lg overflow-hidden border-2">
+                            <Link id="add_issue_link"
+                                  :href="route('issues.create', item.id)">
+                                <div class="p-4 hover:bg-gray-50 hover:text-indigo-400">
+                                    Add a new issue
+                                </div>
+                            </Link>
+
+                            <Link id="item_edit_link"
+                                  :data="item"
+                                  :href="route('items.edit', item.id)">
+                                <div class="p-4 hover:bg-gray-50 hover:text-indigo-400">
+                                    Edit item
+                                </div>
+                            </Link>
+
+                            <button id="item_delete_button"
+                                    type="button"
+                                    @click="destroy(item.id)">
+                                <span class="p-4 hover:bg-gray-50 hover:text-indigo-400">
+                                    Delete item
+                                </span>
+                            </button>
                         </div>
-                    </Link>
-
-                    <Link id="item_edit_link"
-                          :data="item"
-                          :href="route('items.edit', item.id)">
-                        <div class="p-4 hover:bg-gray-50 hover:text-indigo-400">
-                            Edit item
-                        </div>
-                    </Link>
-
-                    <button id="item_delete_button"
-                            type="button"
-                            @click="destroy(item.id)">
-                        <span class="p-4 hover:bg-gray-50 hover:text-indigo-400">
-                            Delete item
-                        </span>
-                    </button>
+                    </div>
                 </div>
-            </div>
-        </div>
+            </template>
+        </ContentStyle>
     </AppLayout>
 </template>
