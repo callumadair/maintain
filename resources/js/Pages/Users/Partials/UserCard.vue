a565d7b543fc6d7cabeac1cd4acd49b54ca335d2
Cal <callum.adair@outlook.com>
2022-09-05 15:10:14 +0100

    Moved the event function content to the parent create issue element.

--- resources/js/Pages/Users/Partials/UserCard.vue
+++ resources/js/Pages/Users/Partials/UserCard.vue
@@ -8,20 +8,13 @@ const props = defineProps({
         required: true,
     }
 });
-
-const greyOut = (user_id: number) => {
-    let user_element = document.getElementById("user" + user_id) as HTMLDivElement;
-    user_element.classList.toggle("bg-gray-50");
-    user_element.classList.toggle("text-indigo-400");
-};
 </script>
 
 <template>
     <div :id="'user' + user.id"
          class="px-4 py-2 rounded-lg text-center hover:bg-gray-50 hover:text-indigo-400">
         <button type="button"
-                @click="greyOut(user.id);
-                $emit('userSelected', user.id)">
+                @click="$emit('userSelected', user.id)">
             {{ user.name }}
         </button>
     </div>
