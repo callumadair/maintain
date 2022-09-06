b70b554d8c31aec1e79a3ef4ce63f1a24c8cf7c1
Cal <callum.adair@outlook.com>
2022-09-06 14:43:08 +0100

    Restructured and restyled the issue show component so that everything is within one content style template.

--- resources/js/Pages/Issues/Show.vue
+++ resources/js/Pages/Issues/Show.vue
@@ -28,51 +28,63 @@ const destroy = (id: number) => Inertia.delete(route('issues.destroy', id));
             </h2>
         </template>
 
-        <ContentStyle v-if="issue.description">
+        <ContentStyle>
             <template #content>
-                {{ issue.description }}
+                <div v-if="issue.description"
+                     id="issue_description"
+                     class="p-2 text-center">
+                    {{ issue.description }}
+                </div>
+
+                <div v-if="issue.description"
+                     class="border-[1px] border-gray-200 rounded-lg"/>
+
+                <div v-if="issue.images.length > 0">
+                    <Gallery :images="issue.images"/>
+                </div>
+
+                <div v-if="issue.images.length > 0"
+                     class="border-[1px] border-gray-200 rounded-lg"/>
+
+                <div class="grid justify-center text-center">
+                    <div class="w-96 grid rounded-lg overflow-hidden border-2">
+                        <label class="p-2"
+                               for="item_link">
+                            Item this problem refers to:
+                        </label>
+                        <div class="border border-1 border-gray-200 rounded-lg"/>
+                        <Link id="item_link"
+                              :href="route('items.show', issue.item_id)"
+                              class="p-2">
+                            <ItemCard :item="issue.item"/>
+                        </Link>
+                    </div>
+                </div>
+
+                <div class="border-[1px] border-gray-200 rounded-lg"/>
+
+                <div class="pb-4 flex justify-center">
+                    <div class="flex rounded-lg overflow-hidden border-2">
+                        <Link id="issue_edit_link"
+                              :data="issue"
+                              :href="route('issues.edit', issue.id)">
+                            <div class="p-4 hover:bg-gray-50 hover:text-indigo-400">
+                                Edit
+                            </div>
+                        </Link>
+
+                        <button id="issue_delete_button"
+                                type="button"
+                                @click="destroy(issue.id)">
+                            <span class="p-4 hover:bg-gray-50 hover:text-indigo-400">
+                                Delete
+                            </span>
+                        </button>
+                    </div>
+                </div>
             </template>
         </ContentStyle>
 
-        <div v-if="issue.images.length > 0">
-            <Gallery :images="issue.images"/>
-        </div>
 
-        <div class="pb-6 pt-8">
-            <div class="grid justify-center text-center">
-                <div class="grid bg-white shadow-xl rounded-lg overflow-hidden w-96">
-                    <label class="p-2"
-                           for="item_link">
-                        Item this problem refers to:
-                    </label>
-                    <div class="border border-1 border-gray-300 rounded-lg"/>
-                    <Link id="item_link"
-                          :href="route('items.show', issue.item_id)"
-                          class="p-2">
-                        <ItemCard :item="issue.item"/>
-                    </Link>
-                </div>
-            </div>
-        </div>
-
-        <div class="py-6">
-            <div class="flex justify-center">
-                <div class="flex bg-white shadow-xl rounded-lg overflow-hidden">
-                    <Link :data="issue"
-                          :href="route('issues.edit', issue.id)">
-                        <div class="p-4 hover:bg-gray-50 hover:text-indigo-400">
-                            Edit
-                        </div>
-                    </Link>
-
-                    <button type="button"
-                            @click="destroy(issue.id)">
-                        <span class="p-4 hover:bg-gray-50 hover:text-indigo-400">
-                            Delete
-                        </span>
-                    </button>
-                </div>
-            </div>
-        </div>
     </AppLayout>
 </template>
