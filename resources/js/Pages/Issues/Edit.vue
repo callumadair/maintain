efc41dacb242a37266eeba3efbeb53c1128babb2
Cal <callum.adair@outlook.com>
2022-09-06 23:47:45 +0100

    Added a horizontal div below the associated users.

--- resources/js/Pages/Issues/Edit.vue
+++ resources/js/Pages/Issues/Edit.vue
@@ -92,6 +92,8 @@ const submit = () => form.post(route('issues.update', props.issue));
                         </div>
                     </div>
 
+                    <div class="border-[1px] border-gray-200 rounded-lg w-full"/>
+
                     <div class="grid space-y-3 justify-center place-items-center">
                         <label class="text-lg"
                                for="issue_title">
