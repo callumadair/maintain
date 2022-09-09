1c5097ef160ae7a45fe3c8b932cc293adbc10952
Cal <callum.adair@outlook.com>
2022-09-09 13:47:46 +0100

    Increased the y spacing.

--- resources/js/Pages/Components/ContentStyle.vue
+++ resources/js/Pages/Components/ContentStyle.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="py-6">
         <div class="max-w-7xl mx-auto px-6">
-            <div class="p-2 bg-white overflow-hidden shadow-xl rounded-lg space-y-6">
+            <div class="p-2 bg-white overflow-hidden shadow-xl rounded-lg space-y-12">
                 <slot name="content"/>
             </div>
         </div>
