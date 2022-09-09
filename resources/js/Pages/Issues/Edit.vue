187cb3c92b55f10aaef56d78d4f1fb7d10efcc0a
Cal <callum.adair@outlook.com>
2022-09-09 15:25:31 +0100

    Added an id to the form in the issue edit component.

--- resources/js/Pages/Issues/Edit.vue
+++ resources/js/Pages/Issues/Edit.vue
@@ -77,7 +77,8 @@ const submit = () => form.post(route('issues.update', props.issue));
 
         <ContentStyle>
             <template #content>
-                <form class="grid grid-cols-1 space-y-12 p-3 m-3 justify-center place-items-center"
+                <form id="issue_edit_form"
+                      class="grid grid-cols-1 space-y-12 p-3 m-3 justify-center place-items-center"
                       @submit.prevent="submit">
 
                     <div id="associated_users"
