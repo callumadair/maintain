e7e79a2ced2c9fb12705db169b9db9c029fbacda
Cal <callum.adair@outlook.com>
2022-09-09 14:57:23 +0100

    Added ids to the gallery.

--- resources/js/Pages/Components/Gallery.vue
+++ resources/js/Pages/Components/Gallery.vue
@@ -12,10 +12,12 @@ const props = defineProps({
 
 <template>
     <div>
-        <div class="my-2 mx-6 flex flex-row flex-wrap justify-center items-center bg-white rounded-lg border-2">
+        <div id="images_gallery"
+             class="my-2 mx-6 flex flex-row flex-wrap justify-center items-center bg-white rounded-lg border-2">
             <div v-for="image in images"
                  class="w-48 m-6">
-                <img :src="image['image_path']"
+                <img :id="'image-' + image.id"
+                     :src="image['image_path']"
                      alt="An image of an item">
             </div>
         </div>
