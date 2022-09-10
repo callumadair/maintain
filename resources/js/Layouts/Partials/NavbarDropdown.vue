cbf18648e45a922e526b30215d7ef724e9b27fda
Cal <callum.adair@outlook.com>
2022-09-11 00:11:33 +0100

    Formatting.

--- resources/js/Layouts/Partials/NavbarDropdown.vue
+++ resources/js/Layouts/Partials/NavbarDropdown.vue
@@ -19,19 +19,17 @@ onUnmounted(() => document.removeEventListener('keydown', closeOnEscape));
              @mouseleave="open = false">
             <slot name="trigger"/>
 
-            <transition
-                enter-active-class="transition ease-out duration-200"
-                enter-from-class="transform opacity-0 scale-95"
-                enter-to-class="transform opacity-100 scale-100"
-                leave-active-class="transition ease-in duration-75"
-                leave-from-class="transform opacity-100 scale-100"
-                leave-to-class="transform opacity-0 scale-95"
-            >
-                <div
-                    v-show="open"
-                    class="absolute z-50 mt-2 rounded-md shadow-lg"
-                    style="display: none;"
-                >
+            <transition enter-active-class="transition ease-out duration-200"
+                        enter-from-class="transform opacity-0 scale-95"
+                        enter-to-class="transform opacity-100 scale-100"
+                        leave-active-class="transition ease-in duration-75"
+                        leave-from-class="transform opacity-100 scale-100"
+                        leave-to-class="transform opacity-0 scale-95">
+
+                <div v-show="open"
+                     class="absolute z-50 mt-2 rounded-md shadow-lg"
+                     style="display: none;">
+
                     <div class="rounded-md ring-1 ring-black ring-opacity-5">
                         <slot name="content"/>
                     </div>
