d69a02765832b35c4c6702253e1408ffebb6b183
Cal <callum.adair@outlook.com>
2022-09-12 15:24:08 +0100

    Created index_functional and index_disabled methods to the item controller to display the functional and disabled items.

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -231,4 +231,52 @@ class ItemController extends Controller
 
         return redirect()->route('items.index')->with('status', 'Item deleted!');
     }
+
+    /**
+     * @return Response
+     */
+    public function index_functional(): Response
+    {
+        $functional_items = Item::query()->where('status', '=', 'Functional')->get();
+        return Inertia::render('Items/Index', [
+            'items' => $functional_items->map(function ($item) {
+                return [
+                    'id' => $item->id,
+                    'name' => $item->name,
+                    'description' => $item->description,
+                    'status' => $item->status,
+                    'user_id' => $item->user_id,
+                    'created_at' => $item->created_at,
+                    'updated_at' => $item->updated_at,
+                    'user' => $item->user,
+                    'issues' => $item->issues,
+                    'images' => $item->images,
+                ];
+            }),
+        ]);
+    }
+
+    /**
+     * @return Response
+     */
+    public function index_disabled(): Response
+    {
+        $disabled_items = Item::query()->where('status', '=', 'Disabled')->get();
+        return Inertia::render('Items/Index', [
+            'items' => $disabled_items->map(function ($item) {
+                return [
+                    'id' => $item->id,
+                    'name' => $item->name,
+                    'description' => $item->description,
+                    'status' => $item->status,
+                    'user_id' => $item->user_id,
+                    'created_at' => $item->created_at,
+                    'updated_at' => $item->updated_at,
+                    'user' => $item->user,
+                    'issues' => $item->issues,
+                    'images' => $item->images,
+                ];
+            }),
+        ]);
+    }
 }
