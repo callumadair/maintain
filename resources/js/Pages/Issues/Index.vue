3509abb078f627881dca859e0f61c8943410562b
Cal <callum.adair@outlook.com>
2022-09-12 22:09:47 +0100

    Added the initial nav to the issue index component for filtering issues.

--- resources/js/Pages/Issues/Index.vue
+++ resources/js/Pages/Issues/Index.vue
@@ -1,6 +1,9 @@
 <script lang="ts" setup>
+import JetNavLink from '@/Jetstream/NavLink.vue';
 import AppLayout from '@/Layouts/AppLayout.vue';
 import IssueCard from '@/Pages/Issues/Partials/IssueCard.vue';
+
+import Book from 'iconoir/icons/book.svg';
 
 import {PropType} from "vue";
 import Issue = App.Models.Issue;
@@ -16,9 +19,21 @@ const props = defineProps({
 <template>
     <AppLayout title="Issue Log">
         <template #header>
-            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            <div class="ml-2">
+                <h2 class="font-semibold text-xl text-gray-800 leading-tight">
                 Issue Log
             </h2>
+
+                <nav class="mt-4 py-2 flex flex-row space-x-6  bg-white">
+                    <img :src="Book"
+                         alt="Issue log icon"/>
+                    <JetNavLink :active="route().current('issues.index')"
+                                :href="route('issues.index')"
+                                class="text-base">
+                        <h3 class="mb-2">All Issues</h3>
+                    </JetNavLink>
+                </nav>
+            </div>
         </template>
 
         <div class="py-6">
