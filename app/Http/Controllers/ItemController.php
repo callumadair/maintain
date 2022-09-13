017194e48e93ab37b3f44fc4bf5cda870c3301f8
Cal <callum.adair@outlook.com>
2022-09-13 14:36:20 +0100

    Re-added 'all' as a default status parameter value in the index method of the item controller.

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -18,10 +18,10 @@ class ItemController extends Controller
     /**
      * Display a listing of the resource.
      *
-     * @param $status
+     * @param string $status
      * @return Response
      */
-    public function index($status): Response
+    public function index(string $status = 'all'): Response
     {
         $items = null;
 
