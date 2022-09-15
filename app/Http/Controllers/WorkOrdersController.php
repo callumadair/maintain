049c1d5d4720b310a5a550c0805f41e6d050186e
Cal <callum.adair@outlook.com>
2022-09-15 18:08:57 +0100

    Created the work order related files.

---
+++ app/Http/Controllers/WorkOrdersController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\WorkOrder;
+use Illuminate\Http\Request;
+
+class WorkOrdersController extends Controller
+{
+    public function index()
+    {
+
+    }
+
+    public function create()
+    {
+    }
+
+    public function store(Request $request)
+    {
+    }
+
+    public function show(WorkOrder $workOrder)
+    {
+    }
+
+    public function edit(WorkOrder $workOrder)
+    {
+    }
+
+    public function update(Request $request, WorkOrder $workOrder)
+    {
+    }
+
+    public function destroy(WorkOrder $workOrder)
+    {
+    }
+}
