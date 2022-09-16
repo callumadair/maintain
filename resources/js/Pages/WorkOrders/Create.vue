a0d905fa6c2544c9e9e0ec02505bc56f288120e8
Cal <callum.adair@outlook.com>
2022-09-17 00:19:42 +0100

    Added a basic template for a work order create component.

---
+++ resources/js/Pages/WorkOrders/Create.vue
@@ -0,0 +1,15 @@
+<script lang="ts" setup>
+import {PropType} from "vue";
+import Issue = App.Models.Issue;
+
+const props = defineProps({
+    issue: {
+        type: Object as PropType<Issue>,
+        required: true,
+    }
+})
+</script>
+
+<template>
+
+</template>
