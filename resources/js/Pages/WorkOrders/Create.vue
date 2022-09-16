acb43dee19e00fb7c8964ebb607b023b26bd1ca3
Cal <callum.adair@outlook.com>
2022-09-17 00:38:43 +0100

    Added basic skeleton of work order create component.

--- resources/js/Pages/WorkOrders/Create.vue
+++ resources/js/Pages/WorkOrders/Create.vue
@@ -1,13 +1,13 @@
 <script lang="ts" setup>
-import {PropType} from "vue";
+import {defineProps, PropType} from "vue";
 import Issue = App.Models.Issue;
 
 const props = defineProps({
     issue: {
         type: Object as PropType<Issue>,
         required: true,
-    }
-})
+    },
+});
 </script>
 
 <template>
