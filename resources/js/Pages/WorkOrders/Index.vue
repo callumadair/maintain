48a9d7f1e32058c6aa47b71feedce26faa6af731
Cal <callum.adair@outlook.com>
2022-09-17 00:38:35 +0100

    Added basic skeleton of work order index component.

---
+++ resources/js/Pages/WorkOrders/Index.vue
@@ -0,0 +1,15 @@
+<script lang="ts" setup>
+import {defineProps, PropType} from "vue";
+import WorkOrder = App.Models.WorkOrder;
+
+const props = defineProps({
+    workOrders: {
+        type: Array as PropType<Array<WorkOrder>>,
+        required: true,
+    },
+});
+</script>
+
+<template>
+
+</template>
