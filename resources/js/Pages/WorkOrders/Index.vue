75326393c39dca0849ce67821cd736696841ea68
Cal <callum.adair@outlook.com>
2022-09-28 16:14:36 +0100

    added the basic template to the work orders index component.

--- resources/js/Pages/WorkOrders/Index.vue
+++ resources/js/Pages/WorkOrders/Index.vue
@@ -1,6 +1,7 @@
 <script lang="ts" setup>
 import {PropType} from "vue";
 import WorkOrder = App.Models.WorkOrder;
+import AppLayout from '@/Layouts/AppLayout.vue';
 
 const props = defineProps({
     workOrders: {
@@ -11,5 +12,13 @@ const props = defineProps({
 </script>
 
 <template>
-
+    <AppLayout title="Work Orders">
+        <template #header>
+            <div class="ml-2">
+                <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                    Work Orders
+                </h2>
+            </div>
+        </template>
+    </AppLayout>
 </template>
