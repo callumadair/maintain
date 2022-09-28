8f76ba6360ed6234d82f7ce0bfc287a9eb679c1b
Cal <callum.adair@outlook.com>
2022-09-28 16:12:07 +0100

    Added passing work orders to the work orders index component.

--- app/Http/Controllers/WorkOrdersController.php
+++ app/Http/Controllers/WorkOrdersController.php
@@ -4,12 +4,28 @@ namespace App\Http\Controllers;
 
 use App\Models\WorkOrder;
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class WorkOrdersController extends Controller
 {
     public function index()
     {
+        $work_orders = WorkOrder::all();
 
+        return Inertia::render('WorkOrders/Index', [
+            'work_orders' => $work_orders->map(function ($work_order) {
+                return [
+                    'id' => $work_order->id,
+                    'requestee_id' => $work_order->requestee_id,
+                    'approvee_id' => $work_order->approvee_id,
+                    'description' => $work_order->description,
+                    'date_wanted' => $work_order->date_wanted,
+                    'total_materials' => $work_order->total_materials,
+                    'total_labour' => $work_order->total_labour,
+                    'total_cost' => $work_order->total_cost,
+                ];
+            }),
+        ]);
     }
 
     public function create()
