1d9f5f80d0535ba6239351628fc2c79b4c6b3830
Cal <callum.adair@outlook.com>
2022-10-01 01:05:08 +0100

    Added basic props to the work order card component.

--- resources/js/Pages/WorkOrders/Partials/WorkOrderCard.vue
+++ resources/js/Pages/WorkOrders/Partials/WorkOrderCard.vue
@@ -1,13 +1,16 @@
+<script lang="ts" setup>
+import {PropType} from "vue";
+import WorkOrder = App.Models.WorkOrder;
+
+const props = defineProps({
+    workOrder: {
+        type: Object as PropType<WorkOrder>,
+        required: true,
+    }
+});
+</script>
+
 <template>
 
 </template>
 
-<script>
-export default {
-    name: "WorkOrderCard"
-}
-</script>
-
-<style scoped>
-
-</style>
