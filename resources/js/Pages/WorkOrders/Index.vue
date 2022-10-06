f276e1edcf9f57969f3aa6fb5b9f1c243677c636
Cal <callum.adair@outlook.com>
2022-10-06 20:53:29 +0100

    Added children of the nav elements.

--- resources/js/Pages/WorkOrders/Index.vue
+++ resources/js/Pages/WorkOrders/Index.vue
@@ -1,7 +1,12 @@
 <script lang="ts" setup>
+import JetNavLink from '@/Jetstream/NavLink.vue';
+import AppLayout from '@/Layouts/AppLayout.vue';
+import WorkOrderCard from "./Partials/WorkOrderCard.vue";
+
 import {PropType} from "vue";
 import WorkOrder = App.Models.WorkOrder;
-import AppLayout from '@/Layouts/AppLayout.vue';
+
+import Page from 'iconoir/icons/page.svg';
 
 const props = defineProps({
     workOrders: {
@@ -18,7 +23,40 @@ const props = defineProps({
                 <h2 class="font-semibold text-xl text-gray-800 leading-tight">
                     Work Orders
                 </h2>
+
+                <nav class="mt-4 py-2 flex flex-row space-x-6">
+                    <img :src="Page"
+                         alt="Page icon"/>
+
+                    <JetNavLink class="text-base">
+                        <h3 class="mb-2">
+                            All Work Orders
+                        </h3>
+                    </JetNavLink>
+
+                    <JetNavLink class="text-base">
+                        <h3 class="mb-2">
+                            Requested
+                        </h3>
+                    </JetNavLink>
+
+                    <JetNavLink class="text-base">
+                        <h3 class="mb-2">
+                            Approved
+                        </h3>
+                    </JetNavLink>
+
+                    <JetNavLink class="text-base">
+                        <h3 class="mb-2">
+                            Completed
+                        </h3>
+                    </JetNavLink>
+                </nav>
             </div>
         </template>
+
+        <div v-for="workOrder in workOrders">
+            <WorkOrderCard :workOrder="workOrder"/>
+        </div>
     </AppLayout>
 </template>
